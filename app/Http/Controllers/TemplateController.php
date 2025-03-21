49577b8f14553ac2cfaa0ea2191e0f44eb359bda
Hadi Hz <hijazih24@gmail.com>
2025-03-21 02:52:37 +0200

    Implement migrations

---
+++ app/Http/Controllers/TemplateController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Template;
+use Illuminate\Http\Request;
+
+class TemplateController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Template $template)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Template $template)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Template $template)
+    {
+        //
+    }
+}
---
+++ database/migrations/2025_03_21_004947_create_values_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Models\Field;
+use App\Models\Record;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('values', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Record::class)->constrained()->cascadeOnDelete();
+            $table->foreignIdFor(Field::class)->constrained()->cascadeOnDelete();
+            $table->text('value');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('values');
+    }
+};
