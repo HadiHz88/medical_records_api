17717de641fe43dce160a13e8bb8f3e8def17332
Hadi Hz <hijazih24@gmail.com>
2025-05-03 14:47:59 +0300

    Add Docs for Models

--- app/Models/Template.php
+++ app/Models/Template.php
@@ -6,26 +6,51 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
+/**
+ * The Template model represents a template in the system.
+ * It includes details such as the template name and description.
+ * A template can have multiple fields, records, and permissions associated with it.
+ */
 class Template extends Model
 {
     /** @use HasFactory<\Database\Factories\TemplateFactory> */
     use HasFactory;
 
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<string>
+     */
     protected $fillable = [
-        'name',
-        'description',
+        'name',        // The name of the template
+        'description', // A brief description of the template
     ];
 
+    /**
+     * Get the fields associated with this template.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
     public function fields(): HasMany
     {
         return $this->hasMany(Field::class);
     }
 
+    /**
+     * Get the records associated with this template.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
     public function records(): HasMany
     {
         return $this->hasMany(Record::class);
     }
 
+    /**
+     * Get the permissions associated with this template.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
     public function permissions(): HasMany
     {
         return $this->hasMany(Permission::class);
